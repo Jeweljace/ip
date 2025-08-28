fb9d1584d454a07177fc60f31f77ca90345b7f83
Jewel Jace Lim <>
2025-08-28 21:29:41 +0800

    ip level 3
    Added Task class to manage list
    Added "mark" and "unmark" functionalities
    mark and unmark command has to be in this format: "mark/unmark<blank><task number>"

--- src/main/java/Helio.java
+++ src/main/java/Helio.java
@@ -2,38 +2,58 @@ import java.util.Scanner;
 
 public class Helio {
     public static void main(String[] args) {
-        String[] userList = new String[100];
+        Task[] userList = new Task[100];
         int count = 0;
         String logo = "        ╱|\n" +
                 "       (˚ˎ 。7\n" +
                 "        |、˜〵\n" +
                 "       じしˍ,)ノ\n";
         System.out.println("____________________________________________________________\n" +
-                "Hello... I'm Helio o.o\n" +
+                " Hello... I'm Helio o.o\n" +
                 logo +
-                "What can I do for you? ._. \n" +
-                "____________________________________________________________\n");
+                " What can I do for you? ._. \n" +
+                "____________________________________________________________");
 
         while (true) {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n");
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________");
                 break;
             } else if (input.equals("list")) {
-                for (String element : userList) {
-                    if (element != null) {
-                        System.out.println(element);
+                System.out.println("____________________________________________________________");
+                if (count == 0) {
+                    System.out.println(" Nothing for meow ^.^");
+                } else {
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println((i + 1) + ". " + userList[i]);
                     }
                 }
-            } else {
-                userList[count] = count + 1 + ". " + input;
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                int taskNum = Integer.parseInt(input.split(" ")[1]);
+                userList[taskNum - 1].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println(" " + userList[taskNum - 1]);
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("unmark ")) {
+                int taskNum = Integer.parseInt(input.split(" ")[1]);
+                userList[taskNum - 1].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println(" " + userList[taskNum - 1]);
+                System.out.println("______________________________________________________________");
+            }
+            else {
+                userList[count] = new Task(input);
                 count++;
                 System.out.println("____________________________________________________________\n" +
                         "added: " + input + "\n" +
-                        "____________________________________________________________\n");
+                        "____________________________________________________________");
             }
 
         }
