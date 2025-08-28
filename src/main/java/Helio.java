f8f0c0c319d31d40fc882ec7f942e771d88dadce
Jewel Jace Lim <>
2025-08-28 20:03:17 +0800

    ip level 1 gave helio some personality added echo functionality and bye command

--- src/main/java/Helio.java
+++ src/main/java/Helio.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+import java.util.List;
+
 public class Helio {
     public static void main(String[] args) {
+        String logo = "        ╱|、\n" +
+                "       (˚ˎ 。7  \n" +
+                "        |、˜〵          \n" +
+                "       じしˍ,)ノ\n";
         System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Helio\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
+                "Hello... I'm Helio o.o\n" +
+                logo +
+                "What can I do for you? ._. \n" +
                 "____________________________________________________________\n");
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            }
+            else {
+                System.out.println("____________________________________________________________\n" +
+                        input +
+                        "\n" +
+                        "____________________________________________________________\n");
+            }
+
+        }
     }
 }
