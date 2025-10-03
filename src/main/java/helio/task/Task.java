552061338551fc9fe52038f43846b1727b166290
Jewel Jace Lim <>
2025-10-03 08:39:15 +0800

    Update Deadline.java and Event.java and Task.java

--- src/main/java/helio/task/Task.java
+++ src/main/java/helio/task/Task.java
@@ -1,8 +1,8 @@
 package helio.task;
 
 public class Task {
-    protected String description;
-    protected boolean isDone;
+    private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
@@ -19,6 +19,14 @@ public class Task {
 
     public void markAsNotDone() {
         isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
     }
 
     public String toSave() {
